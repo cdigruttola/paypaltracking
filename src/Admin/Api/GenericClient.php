f690404cc5cd977aa662e0e6d0f0767d1c11d02a
Carmine Di Gruttola <carmine.di.gruttola@pwc.com>
2023-02-17 17:07:12 +0100

    fix copyright

--- src/Admin/Api/GenericClient.php
+++ src/Admin/Api/GenericClient.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * 2007-2022 Carmine Di Gruttola
+ * Copyright since 2007 Carmine Di Gruttola
  *
  * NOTICE OF LICENSE
  * This source file is subject to the Academic Free License (AFL 3.0)
@@ -18,7 +18,7 @@
  * needs please refer to http://www.prestashop.com for more information.
  *
  * @author    cdigruttola <c.digruttola@hotmail.it>
- * @copyright 2007-2022 Carmine Di Gruttola
+ * @copyright Copyright since 2007 Carmine Di Gruttola
  * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  *
  */
